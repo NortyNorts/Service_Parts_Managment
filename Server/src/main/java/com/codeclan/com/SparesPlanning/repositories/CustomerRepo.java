718ac901f0800da68d2e32009296f997460cf845
Adam Rousseau-Norton <jose_norton@hotmail.com>
2022-07-05 15:11:40 +0100

    First DB Test Complete

--- Server/src/main/java/com/codeclan/com/SparesPlanning/repositories/CustomerRepo.java
+++ Server/src/main/java/com/codeclan/com/SparesPlanning/repositories/CustomerRepo.java
@@ -3,9 +3,11 @@ package com.codeclan.com.SparesPlanning.repositories;
 
 import com.codeclan.com.SparesPlanning.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long>{
-
-
 
 }
