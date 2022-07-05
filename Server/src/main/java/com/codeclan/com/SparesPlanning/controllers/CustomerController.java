672b3fd7fc0d361fb94098e54113bb4de7bee645
Derek Leafgreen <drl21788@gmail.com>
2022-07-05 16:33:55 +0100

    basic routes made

--- Server/src/main/java/com/codeclan/com/SparesPlanning/controllers/CustomerController.java
+++ Server/src/main/java/com/codeclan/com/SparesPlanning/controllers/CustomerController.java
@@ -1,11 +1,17 @@
 package com.codeclan.com.SparesPlanning.controllers;
 
 import com.codeclan.com.SparesPlanning.models.Customer;
+import com.codeclan.com.SparesPlanning.models.Unit;
 import com.codeclan.com.SparesPlanning.repositories.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.ws.Response;
 import java.util.List;
 
 @RestController
@@ -14,9 +20,28 @@ public class CustomerController {
     @Autowired
     CustomerRepo customerRepo;
 
-    @GetMapping(value = "/customer")
+    //get all
+    @GetMapping(value = "/customers")
     public List<Customer> getAllCustomers(){
         return customerRepo.findAll();
     }
+
+    //get one
+    @GetMapping(value="/customers/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id) {
+        return new ResponseEntity(customerRepo.findById(id), HttpStatus.OK);
+    }
+
+    //add one
+    @PostMapping(value="/customers")
+    public ResponseEntity<Customer> postCustomer(@PathVariable Customer customer){
+        customerRepo.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    //find one by unit serial number
+//    @GetMapping(value="/customers/{customerId}/units"){
+//        public ResponseEntity<List<Unit>> getUnitsForCustomer(PathVariable)
+//    }
 
 }
--- Server/src/main/java/com/codeclan/com/SparesPlanning/controllers/PartController.java
+++ Server/src/main/java/com/codeclan/com/SparesPlanning/controllers/PartController.java
@@ -1,9 +1,13 @@
 package com.codeclan.com.SparesPlanning.controllers;
 
+import com.codeclan.com.SparesPlanning.models.Customer;
 import com.codeclan.com.SparesPlanning.models.Part;
 import com.codeclan.com.SparesPlanning.repositories.PartRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +18,18 @@ public class PartController {
         @Autowired
         PartRepo partRepo;
 
-        @GetMapping(value = "/part")
+        //get all
+        @GetMapping(value = "/parts")
         public List<Part> getAllParts(){
             return partRepo.findAll();
         }
+
+        //get one
+        @GetMapping(value="/parts/{id}")
+        public ResponseEntity<Part> getPart(@PathVariable Long id) {
+                return new ResponseEntity(partRepo.findById(id), HttpStatus.OK);
+        }
+
+
 
 }
