1fccc808f786ced23a7f42c4999f5f2c3f5a6fac
Derek Leafgreen <drl21788@gmail.com>
2022-07-08 09:52:17 +0100

    continuing to build routes

--- Server/src/main/java/com/codeclan/com/SparesPlanning/repositories/PartRepo.java
+++ Server/src/main/java/com/codeclan/com/SparesPlanning/repositories/PartRepo.java
@@ -2,6 +2,15 @@ package com.codeclan.com.SparesPlanning.repositories;
 
 import com.codeclan.com.SparesPlanning.models.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PartRepo extends JpaRepository<Part, Long>{
+    @Query("select p from Part p inner join p.units units where units.id = ?1")
+    List<Part> findByUnits_Id(Long id);
 }
+
+
