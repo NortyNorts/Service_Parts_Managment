06c20b1a4914d020bcede2212d9f5e3b57921b79
Derek Leafgreen <drl21788@gmail.com>
2022-07-12 17:40:22 +0100

    more CSS changes

--- client/src/components/service/ServiceCustomerDetails.js
+++ client/src/components/service/ServiceCustomerDetails.js
@@ -7,7 +7,6 @@ import '../../css/main.css'
 const ServiceCustomerDetails = ({currentCustomer, changeUnit, changePartFunction})=>{
     
     const [query, setQuery] = useState("")
-    const [disable, setDisable] = useState(false)
 
     const filteredUnitsArray = currentCustomer.units.filter(unit=>{
         if (query === '') {
@@ -68,7 +67,7 @@ const ServiceCustomerDetails = ({currentCustomer, changeUnit, changePartFunction
             </div>
 
             <div className="link-button">
-                <button disabled={disable} onClick={handleDeleteCustomer}{...() => setDisable(true)}>Delete Customer</button>
+                <button onClick={handleDeleteCustomer}>Delete Customer</button>
             </div>
 
             <div className="search-bar">
